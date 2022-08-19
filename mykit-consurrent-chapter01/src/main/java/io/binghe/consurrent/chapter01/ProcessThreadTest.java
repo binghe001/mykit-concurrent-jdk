18ba728706970aad0a65103b8fce55c5e6fcaa36
binghe001 <1028386804@qq.com>
2022-08-19 15:43:11 +0800

    提交第1章的代码

---
+++ mykit-consurrent-chapter01/src/main/java/io/binghe/consurrent/chapter01/ProcessThreadTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.consurrent.chapter01;
+
+/**
+ * @author binghe
+ * @version 1.0.0
+ * @description 测试进程与线程
+ */
+public class ProcessThreadTest {
+
+    public static void main(String[] args){
+        new Thread(() -> {
+            while (true){
+
+            }
+        },"thread-binghe-001").start();
+        System.out.println("程序启动成功...");
+    }
+}
